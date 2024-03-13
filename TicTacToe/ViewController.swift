b1c258e0c8125d9fb918f63c1ee87b1d528ce06f
Brianca Knight <briancaknight@gmail.com>
2024-03-13 15:17:44 -0700

    Place symbols on grid

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    enum Turn {
+        case Os
+        case Xs
     }
 
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1: UIStackView!
+    @IBOutlet weak var a2: UIStackView!
+    @IBOutlet weak var a3: UIStackView!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    var firstTurn = Turn.Xs
+    var currentTurn = Turn.Xs
+    
+    var OS = "O"
+    var XS = "X"
+    
+    override func viewDidLoad() 
+    {
+        super.viewDidLoad()
+    }
 
+    @IBAction func boardTapAction(_ sender: UIButton) 
+    {
+        addToBoard(sender)
+    }
+
+func addToBoard(_ sender: UIButton)
+{
+    if(sender.title(for: .normal) == nil)
+    {
+        if(currentTurn == Turn.Os)
+        {
+            sender.setTitle(OS, for: .normal)
+            currentTurn = Turn.Xs
+            turnLabel.text = XS
+        }
+        else if(currentTurn == Turn.Xs)
+        {
+            sender.setTitle(XS, for: .normal)
+            currentTurn = Turn.Os
+            turnLabel.text = OS
+        }
+    }
+    }
 }
 

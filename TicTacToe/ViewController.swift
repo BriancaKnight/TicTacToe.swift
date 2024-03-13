0c74495ad213c6e7d8da84da10e94da761488d9e
Brianca Knight <briancaknight@gmail.com>
2024-03-13 16:07:46 -0700

    WIP reset draw

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var turnLabel: UILabel!
-    @IBOutlet weak var a1: UIStackView!
-    @IBOutlet weak var a2: UIStackView!
-    @IBOutlet weak var a3: UIStackView!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var b2: UIButton!
     @IBOutlet weak var b3: UIButton!
@@ -30,15 +30,76 @@ class ViewController: UIViewController {
     
     var OS = "O"
     var XS = "X"
+    var board = [UIButton]()
     
     override func viewDidLoad() 
     {
         super.viewDidLoad()
+        initBoard()
+    }
+    
+    func initBoard()
+    {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) 
     {
         addToBoard(sender)
+        
+        if(fullBoard())
+        {
+            resultAlert(title: "Draw")
+        }
+    }
+    
+    func resultAlert(title: String)
+    {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard()
+    {
+        for button in board
+        {
+            button.setTitle(nil,  for: .normal)
+            button.isEnabled = true
+        }
+        if(firstTurn == Turn.Os)
+        {
+            firstTurn = Turn.Xs
+            turnLabel.text = XS
+        }
+        else if(firstTurn == Turn.Xs)
+            {
+                firstTurn = Turn.Os
+                turnLabel.text = OS
+            }
+        currentTurn = firstTurn
+    }
+    
+    func fullBoard() -> Bool
+    {
+     for button in board
+        {
+         if button.title(for: .normal) == nil
+         {
+             return false
+         }
+     }
+        return true
     }
 
 func addToBoard(_ sender: UIButton)
@@ -57,6 +118,7 @@ func addToBoard(_ sender: UIButton)
             currentTurn = Turn.Os
             turnLabel.text = OS
         }
+        sender.isEnabled = false
     }
     }
 }
